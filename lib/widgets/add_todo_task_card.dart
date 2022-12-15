6275ce0e50699a1b9fc131ada45eceae397e38af
Leonamin <yism810@gmail.com>
2022-12-15 21:38:01 +0900

    추가 - 작업 추가 카드 UI 기본 완성

--- lib/widgets/add_todo_task_card.dart
+++ lib/widgets/add_todo_task_card.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:todolist/constants.dart';
 
@@ -13,6 +15,8 @@ class _AddTodoTaskCardState extends State<AddTodoTaskCard> {
   bool isHover = false;
   bool isActive = false;
 
+  final defaultColor = Colors.black.withOpacity(0.8);
+
   _addTaskCard() {
     return Card(
       color: Colors.transparent,
@@ -24,6 +28,8 @@ class _AddTodoTaskCardState extends State<AddTodoTaskCard> {
         onTap: () {
           setState(() {
             isWriteMode = true;
+            // isHover 상태가 저장되서 작업 취소를 누르면 호버 상태가 유지되는 버그가 발생해서 아래 구문 삽입
+            isHover = false;
           });
         },
         onHover: (value) {
@@ -53,7 +59,7 @@ class _AddTodoTaskCardState extends State<AddTodoTaskCard> {
                 const SizedBox(
                   width: 4,
                 ),
-                // TODO 언어 변경
+                // FIXME 하드코딩 글자
                 Text(
                   "작업 추가",
                   style: TextStyle(
@@ -71,64 +77,214 @@ class _AddTodoTaskCardState extends State<AddTodoTaskCard> {
   }
 
   _makeTaskCard() {
-    return Card(
-      color: Colors.transparent,
-      shadowColor: Colors.transparent,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8),
-        side: BorderSide(
-          width: 1.0,
-          // 가장 하위 우선순위 색상으로
-          color: isActive
-              ? Constants.prioriy4Color
-              : Constants.prioriy4Color.withOpacity(0.5),
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Row(
+    return Column(
+      children: [
+        Card(
+          color: Colors.transparent,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: BorderSide(
+              width: 1.0,
+              // 가장 하위 우선순위 색상으로
+              color: isActive
+                  ? Constants.prioriy4Color
+                  : Constants.prioriy4Color.withOpacity(0.5),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Column(
               children: [
-                Expanded(child: Container()),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      isWriteMode = false;
-                    });
-                  },
-                  // TODO 스타일 변경
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Constants.positiveBackgroundColor),
-                  // TODO 언어 변경
-                  child: Text(
-                    "취소",
-                    // TODO 스타일 변경
-                    style: const TextStyle(color: Colors.black),
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        minLines: 1,
+                        maxLines: 10,
+                        decoration:
+                            const InputDecoration(border: InputBorder.none),
+                        // TODO 글자 크기 정의 작업 제목
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: defaultColor,
+                        ),
+                      ),
+                      TextFormField(
+                        minLines: 1,
+                        maxLines: 10,
+                        decoration:
+                            const InputDecoration(border: InputBorder.none),
+                        // TODO 글자 크기 정의 작업 내용
+                        style: TextStyle(
+                          color: defaultColor,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  width: 8,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  // TODO 스타일 변경
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Constants.positiveColor),
-                  // TODO 언어 변경
-                  child: Text(
-                    "작업 추가",
-                    // TODO 스타일 변경
-                    style: const TextStyle(
-                      color: Constants.positiveBackgroundColor,
+                // 마감 날짜 버튼 및 작업 분류 선택 버튼 행
+                // 마감 날짜 글자는 다보이게 하고 남은 부분을 작업 분류로
+                // 버튼이 호버링한 상태 꾹누른 상태에서 그림자가 안보인다. 눌렀다 떼면 잠깐 보인다.
+                Row(
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.date_range_outlined,
+                            color: defaultColor,
+                          ),
+                          const SizedBox(
+                            width: 4.0,
+                          ),
+                          // FIXME 하드코딩 글자
+                          Text(
+                            "마감 날짜",
+                            style: TextStyle(
+                              color: defaultColor,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Flexible(
+                      child: OutlinedButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Flexible(
+                                  child: Text(
+                                //TODO 섹션 넣기
+                                "프로젝트 글자 초과......",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: defaultColor,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                              Text(
+                                " / ",
+                                style: TextStyle(
+                                  color: defaultColor,
+                                ),
+                              ),
+                              Flexible(
+                                  child: Text(
+                                //TODO 섹션 넣기
+                                "섹션 글자 초과.........",
+                                maxLines: 1,
+                                style: TextStyle(
+                                  color: defaultColor,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ))
+                            ],
+                          )),
+                    ),
+                  ],
+                ),
+                // 기타 아이콘 버튼 행
+                Row(
+                  children: [
+                    // TODO 라벨 기능
+                    InkWell(
+                      onTap: () {},
+                      child: Tooltip(
+                        // FIXME 하드코딩
+                        message: "라벨 설정",
+                        child: Icon(
+                          Icons.label_outline_rounded,
+                          color: defaultColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    // TODO 우선순위 기능
+                    InkWell(
+                      onTap: () {},
+                      child: Tooltip(
+                        // FIXME 하드코딩
+                        message: "우선 순위 설정",
+                        child: Icon(
+                          Icons.flag_outlined,
+                          color: defaultColor,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    // TODO 알람 기능
+                    InkWell(
+                      onTap: () {},
+                      child: Tooltip(
+                        // FIXME 하드코딩
+                        message: "알람 설정",
+                        child: Icon(
+                          Icons.alarm,
+                          color: defaultColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            )
+            ),
+          ),
+        ),
+        // 아래 저장 및 취소 버튼
+        Row(
+          children: [
+            Expanded(child: Container()),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  isWriteMode = false;
+                });
+              },
+              // TODO 스타일 변경
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Constants.positiveBackgroundColor),
+              // FIXME 하드코딩 글자
+              child: Text(
+                "취소",
+                // TODO 스타일 변경
+                style: const TextStyle(color: Colors.black),
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // TODO 작업 추가 기능
+              },
+              // TODO 스타일 변경
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Constants.positiveColor),
+              // FIXME 하드코딩 글자
+              child: Text(
+                "작업 추가",
+                // TODO 스타일 변경
+                style: const TextStyle(
+                  color: Constants.positiveBackgroundColor,
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
           ],
         ),
-      ),
+      ],
     );
   }
 
